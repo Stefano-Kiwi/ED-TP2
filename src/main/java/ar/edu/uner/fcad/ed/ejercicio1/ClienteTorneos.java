193e0f13f13f500cf429897344f3c14d43acf1eb
David Zarcó <davidzarcofca@gmail.com>
2022-04-15 18:46:39 -0300

    Acomodando TorneoAdmin

--- src/main/java/ar/edu/uner/fcad/ed/ejercicio1/ClienteTorneos.java
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio1/ClienteTorneos.java
@@ -10,39 +10,51 @@ public class ClienteTorneos {
     public static void main(String[] args) {
         TorneosAdmin Torneos = new TorneosAdmin();
         
-        Equipo equipo1 = new Equipo("CABJ", new Ciudad("CABA"));
-        Equipo equipo2 = new Equipo("CARP", new Ciudad("CABA"));
-        Equipo equipo3 = new Equipo("CASLA", new Ciudad("CABA"));
-        Equipo equipo4 = new Equipo("FCB", new Ciudad("CABA"));
-        Equipo equipo5 = new Equipo("RMA", new Ciudad("CABA"));
+        Equipo equipo1 = new Equipo("CABJ",new Ciudad("CABA"));
+        Equipo equipo2 = new Equipo("CARP",new Ciudad("CABA"));
+        Equipo equipo3 = new Equipo("CASLA",new Ciudad("CABA"));
+        Equipo equipo4 = new Equipo("FCB",new Ciudad("España"));
+        Equipo equipo5 = new Equipo("RMA",new Ciudad("España"));
+        Equipo equipo6 = new Equipo("RVM",new Ciudad("España"));
   
         
 
         Torneo torneo1 = new Torneo("libertadores", 2020);
         EquipoTorneo equipotorneo1 = new EquipoTorneo(equipo1, torneo1, 12, 2, 6, 4, 2, 3);
-        EquipoTorneo equipotorneo2 = new EquipoTorneo(equipo2, torneo1, 12, 2, 6, 4, 4, 5);
-        EquipoTorneo equipotorneo3 = new EquipoTorneo(equipo3, torneo1, 12, 2, 6, 4, 6, 7);
+        EquipoTorneo equipotorneo2 = new EquipoTorneo(equipo2, torneo1, 12, 10, 0, 2, 4, 5);
+        EquipoTorneo equipotorneo3 = new EquipoTorneo(equipo3, torneo1, 12, 0, 0, 12, 6, 7);
 
-        //administrador.tablaPosiciones(torneo1);
-        /*
-        Segundo torneo
-        
-        */
-        Equipo barsa = new Equipo("barcelona", new Ciudad("Madrid"));
-        Equipo manzana = new Equipo("manzanero", new Ciudad("Concordia"));
-        Equipo salsamora = new Equipo("salseros", new Ciudad("Republica del Congo"));
-        
-        Torneo torneo2 = new Torneo("copaMilk", 2009);
-        EquipoTorneo equipo01 = new EquipoTorneo(barsa, torneo2, 10, 0, 0, 0, 0, 0);
-        EquipoTorneo equipo02 = new EquipoTorneo(manzana, torneo2, 10, 0, 0, 0, 0, 0);
-        EquipoTorneo equipo03 = new EquipoTorneo(salsamora, torneo2, 10, 0, 0, 0, 0, 0);
+  
+        //Segundo torneo
+        Torneo torneo2 = new Torneo("MILK CUP", 2009);
+        EquipoTorneo equipotorneo01 = new EquipoTorneo(equipo4, torneo2, 10, 4, 0, 6, 5, 1);
+        EquipoTorneo equipotorneo02 = new EquipoTorneo(equipo5, torneo2, 10, 2, 3, 6, 2, 2);
+        EquipoTorneo equipotorneo03 = new EquipoTorneo(equipo6, torneo2, 10, 3, 1, 6, 3, 3);
       
-        System.out.println("Cantidad de puntos que le corresponde:" +equipotorneo1.getPuntos());
-        System.out.println("Diferencia entre goles:"+equipotorneo1.getDiferenciaGol());
+        Torneo torneo3 = new Torneo("Copa del mundo",2017);
+        EquipoTorneo equipotorneo001 = new EquipoTorneo(equipo1, torneo3, 15, 1, 10, 4, 4, 4);
+        EquipoTorneo equipotorneo002 = new EquipoTorneo(equipo2, torneo3, 15, 2, 9, 5, 3, 5);
+        EquipoTorneo equipotorneo003 = new EquipoTorneo(equipo5, torneo3, 15, 3, 8, 7, 2, 6);
+          
         Torneos.agregar(equipotorneo1);
         Torneos.agregar(equipotorneo2);
         Torneos.agregar(equipotorneo3);
-        System.out.println("Mayor goleador: "+Torneos.masGoleador());
-        System.out.println("Torneos participados por (Equipo): "+Torneos.todosLosTorneos(new Equipo("CABJ",new Ciudad("CABA"))));
+        Torneos.agregar(equipotorneo01);
+        Torneos.agregar(equipotorneo02);
+        Torneos.agregar(equipotorneo03);
+        Torneos.agregar(equipotorneo001);
+        Torneos.agregar(equipotorneo002);
+        Torneos.agregar(equipotorneo003);
+ 
+        
+        System.out.println("Cantidad de puntos que le corresponde:" +equipotorneo1.getPuntos());
+        System.out.println("Diferencia entre goles:"+equipotorneo1.getDiferenciaGol());
+        System.out.println("Mayor goleador: "+Torneos.masGoleador()); 
+        System.out.println("Equipo al que menos goles le metieron: "+Torneos.vallaMenosVencida());
+        System.out.println("Torneos participados por:(" + equipo1.getNombre() + ")" + Torneos.todosLosTorneos(new Equipo("CABJ",new Ciudad("CABA"))));
+        System.out.println("Tabla de posiciones por parametro:(" + torneo1.getNombre() + ")" + Torneos.tablaPosiciones(torneo1));
+
+        
+        
     }
 }
