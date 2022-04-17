35d3d3c4f840fc71de5dd508280c7c0e85f68683
Stefano-Kiwi <stefanobudon@gmail.com>
2022-04-17 15:38:36 -0300

    Creacion de comparator en Factura y modif de metodos

--- src/main/java/ar/edu/uner/fcad/ed/ejercicio3/FacturaDetalle.java
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio3/FacturaDetalle.java
@@ -10,6 +10,41 @@ public class FacturaDetalle {
     private int cant;
     private double precioUnitario;
     private double subTotal;
+
+    public FacturaDetalle(int numeroItem, Producto producto, int cant, double precioUnitario, double subTotal) {
+        this.numeroItem = numeroItem;
+        this.producto = producto;
+        this.cant = cant;
+        this.precioUnitario = precioUnitario;
+        this.subTotal = subTotal;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.numeroItem;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final FacturaDetalle other = (FacturaDetalle) obj;
+        return this.numeroItem == other.numeroItem;
+    }
+
+    @Override
+    public String toString() {
+        return "FacturaDetalle{" + "numeroItem=" + numeroItem + ", producto=" + producto + ", cant=" + cant + ", precioUnitario=" + precioUnitario + ", subTotal=" + subTotal + '}';
+    }
     
-    
+     
 }
