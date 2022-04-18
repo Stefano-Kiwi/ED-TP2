c28850dc4c415b5f7caffa65e3b7c69660a4c3b0
Stefano-Kiwi <stefanobudon@gmail.com>
2022-04-18 13:59:23 -0300

    top5productos falta terminar iterador

--- src/main/java/ar/edu/uner/fcad/ed/ejercicio3/ClienteScoopsAhoy.java
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio3/ClienteScoopsAhoy.java
@@ -24,12 +24,12 @@ public class ClienteScoopsAhoy {
         LocalDate fecha4 = LocalDate.of(2020, Month.MARCH, 14);
         
         
-        FacturaDetalle facturaDetalle1 = new FacturaDetalle(111,almohada,200,40,80);
-        FacturaDetalle facturaDetalle2 = new FacturaDetalle(22,sommier,2,50,100);
-        FacturaDetalle facturaDetalle3 = new FacturaDetalle(45, pala, 30, 20, 100);
-        FacturaDetalle facturaDetalle4 = new FacturaDetalle(28, monopoly, 1, 15,100);
-        FacturaDetalle facturaDetalle5 = new FacturaDetalle(23, guitarra, 45, 20, 200);
-        FacturaDetalle facturaDetalle6 = new FacturaDetalle(40, violin, 2, 40, 500);
+        FacturaDetalle facturaDetalle1 = new FacturaDetalle(111,almohada,20,40);
+        FacturaDetalle facturaDetalle2 = new FacturaDetalle(22,sommier,2,50);
+        FacturaDetalle facturaDetalle3 = new FacturaDetalle(45, pala, 30, 20);
+        FacturaDetalle facturaDetalle4 = new FacturaDetalle(28, monopoly, 1, 15);
+        FacturaDetalle facturaDetalle5 = new FacturaDetalle(23, guitarra, 45, 20);
+        FacturaDetalle facturaDetalle6 = new FacturaDetalle(40, violin, 2, 40);
         
         
         List<FacturaDetalle> listaFacturaDetalle1 = new ArrayList<FacturaDetalle>();
@@ -44,8 +44,8 @@ public class ClienteScoopsAhoy {
         
         Empresa empresa1 = new Empresa("101", "pepe", SituacionIVAEnum.CONSUMIDOR_FINAL);
         Empresa empresa2 = new Empresa("404", "carlitos", SituacionIVAEnum.RESP_MONOTRIBUTO);
-        Factura factura1 = new Factura(TipoFacturaEnum.FACTURA_A, 1, 01, fecha1,empresa1,listaFacturaDetalle1 , 0);
-        Factura factura2 = new Factura(TipoFacturaEnum.FACTURA_C,2,05,fecha2,empresa2,listaFacturaDetalle2, 2);
+        Factura factura1 = new Factura(TipoFacturaEnum.FACTURA_A, 1, 01, fecha1,empresa1,listaFacturaDetalle1 , 100);
+        Factura factura2 = new Factura(TipoFacturaEnum.FACTURA_C,2,05,fecha2,empresa2,listaFacturaDetalle2, 4000);
         
         
         System.out.println("factura1 = " + factura1+ "\n");
@@ -56,15 +56,15 @@ public class ClienteScoopsAhoy {
         facturaAdmin.add(factura1);
         facturaAdmin.add(factura2);
         
-        
-        facturaAdmin.ordenar();
         System.out.println("facturaAdmin sin ordenar = " + facturaAdmin+ "\n");
-        facturaAdmin.ordenar(new FechaFacturaComparator()); // DEBEMOS IMPLEMENTAR ESTO
+        facturaAdmin.ordenar();
+        System.out.println("facturaAdmin ordenada (según proveedor y num factura): = " + facturaAdmin+ "\n");
+        facturaAdmin.ordenar(new FechaFacturaComparator()); 
         System.out.println("facturaAdmin ordenada por Fecha = " + facturaAdmin+ "\n");
         
         
-        System.out.println("Filtrado por mes y año: "+facturaAdmin.buscarFacturasPorMesAnio(3, 2020)+ "\n");
-        System.out.println("Filtrado por proveedor: "+facturaAdmin.buscarFacturasPorProveedor(empresa1)+ "\n");
+        System.out.println("Filtrado por mes y año (03/2020): "+facturaAdmin.buscarFacturasPorMesAnio(3, 2020)+ "\n");
+        System.out.println("Filtrado por proveedor(empresa1): "+facturaAdmin.buscarFacturasPorProveedor(empresa1)+ "\n");
         System.out.println("Factura con mayor importe total: "+facturaAdmin.facturaMasCostosa()+ "\n");
         //System.out.println("Top 5 productos que más se compraron: "+facturaAdmin.top5ProductoCantidad()); // DEBEMOS IMPLEMENTAR ESTO
     }
